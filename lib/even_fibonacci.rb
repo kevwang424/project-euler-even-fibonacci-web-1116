d7d4e98be5088c64773c373d88d42066171f9d95
kevwang424 <kev.wang424@gmail.com>
2017-01-04 13:44:14 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(num)
+  fib = [1,2]
+  x = 1
+  y = 2
+  while fib[-1] < num && (x + y) < num
+    fib << x + y
+    x = fib[-2]
+    y = fib[-1]
+  end
+
+  sum = 0
+  fib.each do |i|
+    sum += i if i.even?
+  end
+  sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,27 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def sum
+    fib = [1,2]
+    x = 1
+    y = 2
+    while fib[-1] < @num && (x + y) < @num
+      fib << x + y
+      x = fib[-2]
+      y = fib[-1]
+    end
+
+    sum = 0
+    fib.each do |i|
+      sum += i if i.even?
+    end
+    sum
+  end
+
+end
